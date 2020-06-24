7b9e19079f2ff3c8b5abd6aa1baad8242a9fd435
Nikhil Virani <virani.nikhil.g@gmail.com>
2020-06-24 23:10:10 +0530

    Console application created for generating function of stored Procedure

---
+++ DotnetCore.DB/DotnetCore.DB.MethodGenerator/Program.cs
@@ -0,0 +1,44 @@
+﻿using DotnetCore.DB.Extension.Helper;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace DotnetCore.DB.MethodGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConnectionString = @"--Your DB Connection String--";
+            string ContextBasePath = @"--Directory of where context file creation--";
+            string ContextClassName = "--Context Class name--";
+            string ContextFilePath = Path.Combine(ContextBasePath, ContextClassName + ".cs");
+            string NamespaceOfContextFile = "--Namespace Of Context Class--";
+
+            SPInformation storedProcedure = new SPInformation(ConnectionString);
+            var list = storedProcedure.GetSPList().ToList();
+
+            if (list.Count > 0)
+            {
+                GenerateContextFile.Create(
+                    ContextBasePath,
+                    ContextFilePath,
+                    NamespaceOfContextFile,
+                    ContextClassName);
+
+                int i = 0;
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item.name + "- Start");
+                    GenerateFunctions.Methods(ConnectionString, ContextBasePath, ContextFilePath, NamespaceOfContextFile, item.name);
+                    Console.WriteLine(item.name + "- End");
+                    Console.WriteLine("--------------------------------------------------" + i++);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your database has not any stored procedure.");
+            }
+        }
+    }
+}
